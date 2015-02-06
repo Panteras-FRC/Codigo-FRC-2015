3c9db75e0fa36a0e6d2a67753374bf617911d7eb
EnriqueNavarro <enrique1nta@gmail.com>
2015-02-06 17:01:54 -0600

    Conexion con Arduino I2C

--- Arduino/I2C_Slave/I2C_Slave.ino
+++ Arduino/I2C_Slave/I2C_Slave.ino
@@ -1,11 +1,7 @@
 #include <Wire.h>
-#define LIDARLite_ADDRESS 0x62 // Default I2C Address of LIDAR-Lite.
-#define RegisterMeasure 0x00 // Register to write to initiate ranging.
-#define MeasureValue 0x04 // Value to initiate ranging.
-#define RegisterHighLowB 0x8f // Register to get both High and Low bytes in 1 call.
+#define SlaveDeviceId 0x64 // ID Arduino
 
-int reading, readingLo, readingHi = 0;
-const byte SlaveDeviceId = 2;
+int Red, Green, Blue;
 
 void setup()
 {
@@ -13,49 +9,41 @@ void setup()
   Wire.onReceive(receiveCallback);
   Wire.onRequest(requestCallback);
   Serial.begin(9600);
+  Serial.println("Enabled!");
 }
 
 void loop()
 {
-  sensorRead();
 }
 
 // aCount is the number of bytes received.
 void receiveCallback(int aCount)
 {
-  if(aCount == 4)
-  {
-    char receivedValue = Wire.read() << 8;
-    receivedValue |= Wire.read();
-    Serial.println(receivedValue);
-  }
-  else
-  {
-    Serial.print("Unexpected number of bytes received: ");
-    Serial.println(aCount);
+  int registerValue = Wire.read();
+  switch (registerValue){
+  case 0x0:
+    Red = Wire.read();
+    Green = Wire.read();
+    Blue = Wire.read();
+    Serial.println(Red);
+    Serial.println(Green);
+    Serial.println(Blue);
+    break;
+  case 0x01:
+    char message;
+    for(int i = 0; i <= (aCount-1); i++){
+      message = message + Wire.read();
+    }
+    Serial.println(message);
+    break;
+  default:
+    break;
   }
 }
 
 void requestCallback(){
-  Serial.println("data requested");
-  Wire.write(reading);
+  Serial.println("Data Requested");
 }
 
-void sensorRead(){
-  Wire.beginTransmission((int)LIDARLite_ADDRESS); // transmit to LIDAR-Lite
-  Wire.write((int)RegisterMeasure); // sets register pointer to (0x00)
-  Wire.write((int)MeasureValue); // sets register pointer to (0x00)
-  Wire.endTransmission(); // stop transmitting
-  delay(20); // Wait 20ms for transmit
-  Wire.beginTransmission((int)LIDARLite_ADDRESS); // transmit to LIDAR-Lite
-  Wire.write((int)RegisterHighLowB); // sets register pointer to (0x8f)
-  Wire.endTransmission(); // stop transmitting
-  delay(20); // Wait 20ms for transmit
-  Wire.requestFrom((int)LIDARLite_ADDRESS, 2); // request 2 bytes from LIDAR-Lite
-  if(2 <= Wire.available()){ // if two bytes were received
-    readingHi = Wire.read(); // receive high byte (overwrites previous reading)
-    readingLo = Wire.read(); // receive low byte as lower 8 bits
-    reading = (readingHi << 8) + readingLo;
-    Serial.println(reading); // print the reading
-  }
-}
+
+
